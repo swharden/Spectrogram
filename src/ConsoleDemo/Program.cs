d956ee63445666729baa33863af790db948cbdf7
Scott W Harden <swharden@gmail.com>
2019-08-16 16:55:16 -0400

    working FFT

---
+++ src/ConsoleDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] values = Spectrogram.Tools.generateFakeSignal();
+            double[] fft = Spectrogram.Tools.FFT(values);
+
+            Spectrogram.Tools.plotValues(values);
+            Spectrogram.Tools.plotFFT(fft);
+
+            Console.WriteLine("DONE");
+        }
+    }
+}
