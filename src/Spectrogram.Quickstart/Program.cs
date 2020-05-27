d60aa59940914338e9bc58f9871757cab190041b
Scott W Harden <swharden@gmail.com>
2020-05-27 19:33:33 -0400

    create new projects

---
+++ src/Spectrogram.Quickstart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spectrogram.Quickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spec = new Spectrogram();
+        }
+    }
+}
