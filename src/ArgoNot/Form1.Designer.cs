5a420766cdb3700d5d03db53db741b3ba6ad861f
Scott W Harden <swharden@gmail.com>
2020-06-02 18:25:27 -0400

    argonot

---
+++ src/ArgoNot/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace ArgoNot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlSpectrogram = new System.Windows.Forms.Panel();
+            this.pbSpectrogram = new System.Windows.Forms.PictureBox();
+            this.cbColormap = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbWindow = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbSoundCard = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pnlAmplitude = new System.Windows.Forms.Panel();
+            this.pbLevel = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.nudBrightness = new System.Windows.Forms.NumericUpDown();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.pnlSpectrogram.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpectrogram)).BeginInit();
+            this.pnlAmplitude.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLevel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBrightness)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSpectrogram
+            // 
+            this.pnlSpectrogram.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlSpectrogram.AutoScroll = true;
+            this.pnlSpectrogram.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlSpectrogram.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSpectrogram.Controls.Add(this.pbSpectrogram);
+            this.pnlSpectrogram.Location = new System.Drawing.Point(12, 51);
+            this.pnlSpectrogram.Name = "pnlSpectrogram";
+            this.pnlSpectrogram.Size = new System.Drawing.Size(1060, 418);
+            this.pnlSpectrogram.TabIndex = 2;
+            // 
+            // pbSpectrogram
+            // 
+            this.pbSpectrogram.BackColor = System.Drawing.Color.Navy;
+            this.pbSpectrogram.Location = new System.Drawing.Point(0, 0);
+            this.pbSpectrogram.Name = "pbSpectrogram";
+            this.pbSpectrogram.Size = new System.Drawing.Size(100, 50);
+            this.pbSpectrogram.TabIndex = 0;
+            this.pbSpectrogram.TabStop = false;
+            // 
+            // cbColormap
+            // 
+            this.cbColormap.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbColormap.FormattingEnabled = true;
+            this.cbColormap.Items.AddRange(new object[] {
+            "Viridis",
+            "Grayscale",
+            "Plasma",
+            "Argo"});
+            this.cbColormap.Location = new System.Drawing.Point(265, 24);
+            this.cbColormap.Name = "cbColormap";
+            this.cbColormap.Size = new System.Drawing.Size(121, 21);
+            this.cbColormap.TabIndex = 4;
+            this.cbColormap.SelectedIndexChanged += new System.EventHandler(this.cbColormap_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Colormap";
+            // 
+            // cbWindow
+            // 
+            this.cbWindow.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbWindow.FormattingEnabled = true;
+            this.cbWindow.Items.AddRange(new object[] {
+            "Rectangular",
+            "Hanning",
+            "Blackman",
+            "FlatTop"});
+            this.cbWindow.Location = new System.Drawing.Point(392, 24);
+            this.cbWindow.Name = "cbWindow";
+            this.cbWindow.Size = new System.Drawing.Size(121, 21);
+            this.cbWindow.TabIndex = 6;
+            this.cbWindow.SelectedIndexChanged += new System.EventHandler(this.cbWindow_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(390, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Window";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Sound Card";
+            // 
+            // cbSoundCard
+            // 
+            this.cbSoundCard.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSoundCard.FormattingEnabled = true;
+            this.cbSoundCard.Location = new System.Drawing.Point(12, 24);
+            this.cbSoundCard.Name = "cbSoundCard";
+            this.cbSoundCard.Size = new System.Drawing.Size(121, 21);
+            this.cbSoundCard.TabIndex = 9;
+            this.cbSoundCard.SelectedIndexChanged += new System.EventHandler(this.cbSoundCard_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(968, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 30);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ArgoNOT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(516, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Brightness";
+            // 
+            // pnlAmplitude
+            // 
+            this.pnlAmplitude.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pnlAmplitude.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlAmplitude.Controls.Add(this.pbLevel);
+            this.pnlAmplitude.Location = new System.Drawing.Point(139, 24);
+            this.pnlAmplitude.Name = "pnlAmplitude";
+            this.pnlAmplitude.Size = new System.Drawing.Size(120, 21);
+            this.pnlAmplitude.TabIndex = 14;
+            // 
+            // pbLevel
+            // 
+            this.pbLevel.BackColor = System.Drawing.Color.Green;
+            this.pbLevel.Location = new System.Drawing.Point(0, 0);
+            this.pbLevel.Name = "pbLevel";
+            this.pbLevel.Size = new System.Drawing.Size(60, 21);
+            this.pbLevel.TabIndex = 1;
+            this.pbLevel.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(136, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Audio Level";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // nudBrightness
+            // 
+            this.nudBrightness.DecimalPlaces = 1;
+            this.nudBrightness.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudBrightness.Location = new System.Drawing.Point(519, 26);
+            this.nudBrightness.Name = "nudBrightness";
+            this.nudBrightness.Size = new System.Drawing.Size(69, 20);
+            this.nudBrightness.TabIndex = 16;
+            this.nudBrightness.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudBrightness.ValueChanged += new System.EventHandler(this.nudBrightness_ValueChanged);
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(735, 9);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(132, 30);
+            this.lblTime.TabIndex = 17;
+            this.lblTime.Text = "12:34:56 UTC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 481);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.pnlAmplitude);
+            this.Controls.Add(this.pnlSpectrogram);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudBrightness);
+            this.Controls.Add(this.cbSoundCard);
+            this.Controls.Add(this.cbWindow);
+            this.Controls.Add(this.cbColormap);
+            this.Name = "Form1";
+            this.Text = "ArgoNot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlSpectrogram.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpectrogram)).EndInit();
+            this.pnlAmplitude.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLevel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBrightness)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlSpectrogram;
+        private System.Windows.Forms.ComboBox cbColormap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbWindow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbSoundCard;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel pnlAmplitude;
+        private System.Windows.Forms.PictureBox pbLevel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox pbSpectrogram;
+        private System.Windows.Forms.NumericUpDown nudBrightness;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ src/ArgoNot/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArgoNot
+{
+    public partial class Form1 : Form
+    {
+        private Spectrogram.SpectrogramLive spec;
+        private NAudio.Wave.WaveInEvent wvin;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbColormap.SelectedIndex = 0;
+            cbWindow.SelectedIndex = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ScanAudioDevices();
+        }
+
+        private void cbSoundCard_SelectedIndexChanged(object sender, EventArgs e) => StartAudioMonitor();
+
+        private void ScanAudioDevices()
+        {
+            if (NAudio.Wave.WaveIn.DeviceCount == 0)
+            {
+                MessageBox.Show("No audio input devices found.\n\nThis program will now exit.",
+                    "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+
+            cbSoundCard.Items.Clear();
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+                cbSoundCard.Items.Add(NAudio.Wave.WaveIn.GetCapabilities(i).ProductName);
+            cbSoundCard.SelectedIndex = 0;
+        }
+
+        private void StartAudioMonitor()
+        {
+            int sampleRate = 6000; // 48kHz / 8
+            int fftSize = 1 << 14; // 16384 (2.7 sec)
+            int widthPx = 1000;
+            int stepSize = sampleRate * 60 * 10 / widthPx; // 10 minutes
+
+            // create spectrogram
+            spec = new Spectrogram.SpectrogramLive(sampleRate, fftSize, stepSize, widthPx,
+                freqMin: 0, freqMax: 3000);
+            spec.pixelMult = (double)nudBrightness.Value;
+
+            double startTimeSec = (DateTime.UtcNow.Minute % 10) * 60 + DateTime.UtcNow.Second;
+            int stepsToSkip = (int)(startTimeSec * sampleRate / stepSize);
+            spec.SetNextIndex(stepsToSkip);
+
+            // apply spectrogram Bitmap to picturebox
+            pbSpectrogram.Image?.Dispose();
+            pbSpectrogram.Image = spec.GetBitmap();
+            pbSpectrogram.Size = pbSpectrogram.Image.Size;
+
+            // start sound card listener
+            wvin?.StopRecording();
+            wvin?.Dispose();
+            wvin = new NAudio.Wave.WaveInEvent
+            {
+                DeviceNumber = cbSoundCard.SelectedIndex,
+                WaveFormat = new NAudio.Wave.WaveFormat(sampleRate, bits: 16, channels: 1),
+                BufferMilliseconds = 20
+            };
+            wvin.DataAvailable += OnDataAvailable;
+            wvin.StartRecording();
+        }
+
+        private static double amplitudeFrac = 0;
+        private void OnDataAvailable(object sender, NAudio.Wave.WaveInEventArgs args)
+        {
+            int bytesPerSample = wvin.WaveFormat.BitsPerSample / 8;
+            int samplesRecorded = args.BytesRecorded / bytesPerSample;
+            double[] buffer = new double[samplesRecorded];
+            double peak = 0;
+            for (int i = 0; i < samplesRecorded; i++)
+            {
+                buffer[i] = BitConverter.ToInt16(args.Buffer, i * bytesPerSample);
+                peak = Math.Max(peak, buffer[i]);
+            }
+            amplitudeFrac = peak / (1 << 15);
+
+            // process FFT as data comes in
+            spec.Extend(buffer, process: false);
+        }
+
+        private void nudBrightness_ValueChanged(object sender, EventArgs e)
+        {
+            spec.pixelMult = (double)nudBrightness.Value;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // set audio level meter
+            int newWidth = (int)(amplitudeFrac * pnlAmplitude.Width);
+            if (newWidth < pbLevel.Width)
+                pbLevel.Width -= 1;
+            else
+                pbLevel.Width = newWidth;
+        }
+
+        int minutePrevious = -1;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            var now = DateTime.UtcNow;
+
+            // determine when 10-minute rollovers occur
+            int minuteNow = now.Minute;
+            if (minuteNow != minutePrevious)
+            {
+                minutePrevious = minuteNow;
+                if (minuteNow % 10 == 0)
+                {
+                    spec.SetNextIndex();
+                    spec.GetBitmap().Save($"{DateTime.UtcNow.Ticks}.bmp");
+                }
+            }
+
+            lblTime.Text = $"{now.Hour:D2}:{now.Minute:D2}:{now.Second:D2} UTC";
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            // force a render
+            spec.ProcessAll();
+            pbSpectrogram.Image?.Dispose();
+            pbSpectrogram.Image = spec.GetBitmap(highlightIndex: true);
+        }
+
+        private void cbColormap_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (spec is null)
+                return;
+
+            if (cbColormap.Text == "Viridis")
+                new Spectrogram.Colormaps.Viridis().Apply(spec.GetBitmap());
+            else if (cbColormap.Text == "Grayscale")
+                new Spectrogram.Colormaps.Grayscale().Apply(spec.GetBitmap());
+            else if (cbColormap.Text == "Plasma")
+                new Spectrogram.Colormaps.Plasma().Apply(spec.GetBitmap());
+            else if (cbColormap.Text == "Argo")
+                new Spectrogram.Colormaps.Grayscale().Apply(spec.GetBitmap());
+        }
+
+        private void cbWindow_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (spec is null)
+                return;
+
+            // TODO: build this into FFTsharp
+            double[] rectangular = new double[spec.fftSize];
+            for (int i = 0; i < rectangular.Length; i++)
+                rectangular[i] = 1;
+
+            if (cbWindow.Text == "Rectangular")
+                spec.SetWindow(rectangular);
+            else if (cbWindow.Text == "Hanning")
+                spec.SetWindow(FftSharp.Window.Hanning(spec.fftSize));
+            else if (cbWindow.Text == "Blackman")
+                spec.SetWindow(FftSharp.Window.Blackman(spec.fftSize));
+            else if (cbWindow.Text == "FlatTop")
+                spec.SetWindow(FftSharp.Window.FlatTop(spec.fftSize));
+        }
+    }
+}
