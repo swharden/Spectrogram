88c94bf1fde5064727a2e68ff4b9c6f0e1e241f8
Scott W Harden <swharden@gmail.com>
2020-06-07 20:02:06 -0400

    colormap investigation

---
+++ dev/colormap/analyzeImage.py
@@ -0,0 +1,30 @@
+"""
+This script reverse-engineers the pallette for a spectrogram.
+It takes in an image, explodes it into pixels, finds all the
+unique values, then attempts to sort them by intensity.
+"""
+
+from matplotlib.image import imread
+import numpy as np
+
+
+def rgbToInt32(r, g, b):
+    return int(r * 2**16 + g * 2**8 + b)
+
+
+def valuesInImage(filePath):
+    im = imread(filePath)
+    pixelValues = np.zeros((im.shape[0], im.shape[1]), dtype=int)
+    for y in range(im.shape[0]):
+        for x in range(im.shape[1]):
+            r, g, b = im[y, x]
+            pixelValues[y, x] = rgbToInt32(r * 255, g * 255, b * 255)
+    pixelValues = np.unique(pixelValues)
+    print(pixelValues)
+    print(f"Found {len(pixelValues)} unique pixel values")
+
+
+if __name__ == "__main__":
+    assert rgbToInt32(111, 222, 121) == 7331449
+    pixelValues = valuesInImage("analyzed/argo1.png")
+    print("DONE")
--- dev/colormap/mp.py
+++ dev/colormap/mp.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pyperclip
+import numpy as np
+
 
 def cmapToCode(cmapName="inferno"):
     cmap = plt.get_cmap(cmapName)
@@ -16,8 +18,66 @@ def cmapToCode(cmapName="inferno"):
     print(f"Colormap {cmapName} copied to clipboard.")
 
 
+def rgbToInt32(r, g, b):
+    return int(r * 2**16 + g * 2**8 + b)
+
+
+def cmapToInt32s(cmapName="viridis"):
+    cmap = plt.get_cmap(cmapName)
+    for i in range(256):
+        r, g, b, a = cmap(i/255.0)
+        int32 = rgbToInt32(r*255, g*255, b*255)
+        print(f"{int32:010d}, ", end='')
+        if i % 8 == 7:
+            print()
+
+
+def plotCmapCurves(cmapName="viridis"):
+    cmap = plt.get_cmap(cmapName)
+    rgbValues = np.zeros((256, 4))
+    for i in range(256):
+        r, g, b, a = cmap(i/255.0)
+        rgbValues[i, 0] = r*255
+        rgbValues[i, 1] = g*255
+        rgbValues[i, 2] = b*255
+        rgbValues[i, 3] = np.sum(rgbValues[i])
+
+    plt.figure(figsize=(6, 4))
+    plt.grid(alpha=.2, ls='--')
+    plt.plot(rgbValues[:, 0], 'r-')
+    plt.plot(rgbValues[:, 1], 'g-')
+    plt.plot(rgbValues[:, 2], 'b-')
+    plt.plot(rgbValues[:, 3]/3, 'k:')
+    plt.ylabel("Intensity by Color")
+    plt.xlabel("Signal Intensity")
+    plt.title(f"{cmapName} colormap")
+
+
+def colormapToIntegerText(cmapName="viridis"):
+    txt = f"# colormap {cmapName}\n"
+
+    cmap = plt.get_cmap(cmapName)
+    for i in range(256):
+        r, g, b, a = cmap(i/255.0)
+        int32 = rgbToInt32(r*255, g*255, b*255)
+        int32 *= 255 # alpha
+        txt += f"{int32:010d}, "
+        if i % 8 == 7:
+            txt += "\n"
+
+    with open(f"analyzed2/{cmapName}.csv", 'w') as f:
+        f.write(txt)
+
+
 if __name__ == "__main__":
-    cmapToCode("cividis")
-    # maps = sorted(m for m in plt.cm.datad if not m.endswith("_r"))
-    # for cmap in maps:
-    #     cmapToCode(cmap)
+    cmapNames = ["cividis", "gray", "inferno",
+                 "magma", "plasma", "viridis"]
+
+    for cmapName in cmapNames:
+        print(f"analyzing {cmapName}...")
+        colormapToIntegerText(cmapName)
+        plotCmapCurves(cmapName)
+        plt.savefig(f"analyzed2/{cmapName}.png")
+        plt.close()
+
+print("DONE")
