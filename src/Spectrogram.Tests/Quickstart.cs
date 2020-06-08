a76a3a921f55ece33f1f2b5a8e81bbd629858029
Scott W Harden <swharden@gmail.com>
2020-06-07 22:48:51 -0400

    add song quickstart to tests

--- src/Spectrogram.Tests/Quickstart.cs
+++ src/Spectrogram.Tests/Quickstart.cs
@@ -8,15 +8,39 @@ namespace Spectrogram.Tests
     public class Quickstart
     {
         [Test]
-        public void Test_WholeFile_DefaultSettings()
+        public void Test_Quickstart_Hal()
         {
             double[] audio = Read.WavInt16mono("../../../../../data/cant-do-that-44100.wav");
+            int sampleRate = 44100;
 
-            var spec = new Spectrogram(sampleRate: 44100, fftSize: 1 << 12, stepSize: 500, maxFreq: 3000);
+            int fftSize = 4096;
+            var spec = new Spectrogram(sampleRate, fftSize, stepSize: 500, maxFreq: 3000);
             spec.Add(audio);
             spec.SaveImage("../../../../../dev/graphics/hal.png", intensity: .2);
             
             Console.WriteLine(spec);
+        }
+
+        [Test]
+        public void Test_Quickstart_Handel()
+        {
+            double[] audio = Mp3.Read("../../../../../data/Handel - Air and Variations.mp3");
+            int sampleRate = 44100;
+
+            int fftSize = 16384;
+            int targetWidthPx = 3000;
+            int stepSize = audio.Length / targetWidthPx;
+
+            var spec = new Spectrogram(sampleRate, fftSize, stepSize, maxFreq: 2200);
+            spec.Add(audio);
+            spec.SaveImage("../../../../../dev/spectrogram-song.jpg", intensity: 5, dB: true);
+
+            Console.WriteLine(spec);
+            /*
+             Spectrogram (2993, 817)
+               Vertical (817 px): 0 - 2,199 Hz, FFT size: 16,384 samples, 2.69 Hz/px
+               Horizontal (2993 px): 2.96 min, window: 0.37 sec, step: 0.06 sec, overlap: 84%
+            */
         }
     }
 }
