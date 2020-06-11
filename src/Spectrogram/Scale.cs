46349364c15e19f66d8648121d39185d321d6c39
Scott W Harden <swharden@gmail.com>
2020-06-11 18:32:28 -0400

    pxPerPx argument becomes reduction

--- src/Spectrogram/Scale.cs
+++ src/Spectrogram/Scale.cs
@@ -9,7 +9,7 @@ namespace Spectrogram
 {
     static class Scale
     {
-        public static Bitmap Vertical(int width, Settings settings, int offsetHz = 0, int tickSize = 3, int pxPerPx = 1)
+        public static Bitmap Vertical(int width, Settings settings, int offsetHz = 0, int tickSize = 3, int reduction = 1)
         {
             double tickHz = 1;
             int minSpacingPx = 50;
@@ -20,11 +20,11 @@ namespace Spectrogram
                 tickHz *= multipliers[multiplier++ % multipliers.Length];
                 double tickCount = settings.FreqSpan / tickHz;
                 double pxBetweenTicks = settings.Height / tickCount;
-                if (pxBetweenTicks >= minSpacingPx * pxPerPx)
+                if (pxBetweenTicks >= minSpacingPx * reduction)
                     break;
             }
 
-            Bitmap bmp = new Bitmap(width, settings.Height / pxPerPx, PixelFormat.Format32bppPArgb);
+            Bitmap bmp = new Bitmap(width, settings.Height / reduction, PixelFormat.Format32bppPArgb);
 
             using (var gfx = Graphics.FromImage(bmp))
             using (var pen = new Pen(Color.Black))
@@ -48,7 +48,7 @@ namespace Spectrogram
 
                 foreach (var freq in freqs)
                 {
-                    int y = settings.PixelY(freq) / pxPerPx;
+                    int y = settings.PixelY(freq) / reduction;
                     gfx.DrawLine(pen, 0, y, tickSize, y);
                     gfx.DrawString($"{freq + offsetHz:N0} Hz", font, brush, tickSize, y, sf);
                 }
