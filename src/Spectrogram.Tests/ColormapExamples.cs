287cd12fbcfa400ae0d8feb6d15d7b8c34659227
Scott W Harden <swharden@gmail.com>
2020-06-11 23:44:45 -0400

    add test to generate all colormaps

--- src/Spectrogram.Tests/ColormapExamples.cs
+++ src/Spectrogram.Tests/ColormapExamples.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using Spectrogram.Colormaps;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Text;
 
@@ -10,32 +11,22 @@ namespace Spectrogram.Tests
 {
     class ColormapExamples
     {
-        private void HalSpectrogram(Colormap cmap)
+        [Test]
+        public void Test_Make_CommonColormaps()
         {
             double[] audio = Read.WavInt16mono("../../../../../data/cant-do-that-44100.wav");
             int sampleRate = 44100;
-            int fftSize = 1 << 13;
-            var spec = new Spectrogram(sampleRate, fftSize, stepSize: 200, maxFreq: 3000);
-            spec.SetWindow(FftSharp.Window.Hanning(fftSize/3)); // sharper window than typical
+            int fftSize = 1 << 12;
+            var spec = new Spectrogram(sampleRate, fftSize, stepSize: 700, maxFreq: 2000);
+            spec.SetWindow(FftSharp.Window.Hanning(fftSize / 3)); // sharper window than typical
             spec.Add(audio);
-            spec.SetColormap(cmap);
-            spec.SaveImage($"../../../../../dev/graphics/hal-{cmap.Name}.png", intensity: .5);
-        }
-        [Test]
 
-        public void Test_Make_CommonColormaps()
-        {
-            HalSpectrogram(Colormap.Argo);
-            HalSpectrogram(Colormap.Blues);
-            HalSpectrogram(Colormap.Cividis);
-            HalSpectrogram(Colormap.Grayscale);
-            HalSpectrogram(Colormap.GrayscaleReversed);
-            HalSpectrogram(Colormap.Greens);
-            HalSpectrogram(Colormap.Inferno);
-            HalSpectrogram(Colormap.Jet);
-            HalSpectrogram(Colormap.Magma);
-            HalSpectrogram(Colormap.Plasma);
-            HalSpectrogram(Colormap.Viridis);
+            foreach(var cmap in Colormap.GetColormaps())
+            {
+                spec.SetColormap(cmap);
+                spec.SaveImage($"../../../../../dev/graphics/hal-{cmap.Name}.png", intensity: .5);
+                Debug.WriteLine($"![](dev/graphics/hal-{cmap.Name}.png)");
+            }
         }
     }
 }
