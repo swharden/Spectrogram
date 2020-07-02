9ece34521fc04e797348460c2dce8519f9a7ee31
Scott W Harden <swharden@gmail.com>
2020-07-02 19:19:12 -0400

    simplify microphone demo

--- src/Spectrogram.MicrophoneDemo/Program.cs
+++ src/Spectrogram.MicrophoneDemo/Program.cs
@@ -16,7 +16,7 @@ namespace Spectrogram.MicrophoneDemo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMenu());
+            Application.Run(new FormMicrophone());
         }
     }
 }
