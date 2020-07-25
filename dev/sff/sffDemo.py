2b5d67a8e3758d84c509fff780aadd60e8699cf3
Scott W Harden <swharden@gmail.com>
2020-07-24 21:30:19 -0400

    simplify SFF demo

--- dev/sff/sffDemo.py
+++ dev/sff/sffDemo.py
@@ -1,15 +1,21 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+np.set_printoptions(precision=3)
+
 import sffLib
 
 def plotSFF(filePath, show = False):
 
+    # access SFF details and data like this
     sf = sffLib.SpectrogramFile(filePath)
+    print(sf.getDescription())
+    print(sf.values)
 
     # rotate values for display as a pseudocolor mesh
     rotatedValues = np.rot90(sf.values, 1)[::-1]
 
+    # plot the spectrogram    
     plt.figure()
     plt.pcolormesh(rotatedValues)
     plt.colorbar()
@@ -21,6 +27,6 @@ def plotSFF(filePath, show = False):
 
 if __name__ == "__main__":
     plotSFF("hal.sff")
-    plotSFF("halMel.sff")
+    #plotSFF("halMel.sff")
 
 
