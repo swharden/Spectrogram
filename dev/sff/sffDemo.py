65116e79cb576ed207a491dd9ba8759584385fc2
Scott W Harden <swharden@gmail.com>
2020-07-24 21:05:52 -0400

    restructure Python SFF demo
    #19

---
+++ dev/sff/sffDemo.py
@@ -0,0 +1,24 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import sffLib
+
+def plotSFF(filePath, show = False):
+
+    sf = sffLib.SpectrogramFile(filePath)
+
+    # rotate values for display as a pseudocolor mesh
+    rotatedValues = np.rot90(sf.values, 1)[::-1]
+    plt.pcolormesh(rotatedValues)
+
+    plt.colorbar()
+    plt.title(f"{filePath} Spectrogram")
+    plt.savefig(filePath.replace(".sff", ".png"))
+    if show:
+        plt.show()
+
+if __name__ == "__main__":
+    plotSFF("hal.sff")
+    plotSFF("halMel.sff")
+
+
