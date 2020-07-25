0cfc9363310faf569438e66ff08b003486dea6a3
Scott W Harden <swharden@gmail.com>
2020-07-24 21:06:59 -0400

    fix duplicate colorbar

--- dev/sff/sffDemo.py
+++ dev/sff/sffDemo.py
@@ -9,13 +9,15 @@ def plotSFF(filePath, show = False):
 
     # rotate values for display as a pseudocolor mesh
     rotatedValues = np.rot90(sf.values, 1)[::-1]
-    plt.pcolormesh(rotatedValues)
 
+    plt.figure()
+    plt.pcolormesh(rotatedValues)
     plt.colorbar()
     plt.title(f"{filePath} Spectrogram")
     plt.savefig(filePath.replace(".sff", ".png"))
     if show:
         plt.show()
+    plt.close()
 
 if __name__ == "__main__":
     plotSFF("hal.sff")
