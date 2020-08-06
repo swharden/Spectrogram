f1c8b15c09134d42b43a1b7f8573206a4840147c
Scott W Harden <swharden@gmail.com>
2020-08-05 22:17:48 -0400

    Create TestAGC.cs
    explore AGC options https://github.com/swharden/FSKview/issues/15

---
+++ src/Spectrogram.Tests/TestAGC.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spectrogram.Tests
+{
+    class TestAGC
+    {
+        //[Test]
+        public void Test_QRSS_noAGC()
+        {
+            string wavFilePath = "../../../../../data/qrss-10min.wav";
+            (int sampleRate, double[] L) = WavFile.ReadMono(wavFilePath);
+            
+
+            int fftSize = 8192;
+            var spec = new Spectrogram(sampleRate, fftSize, stepSize: 2000, maxFreq: 3000);
+            spec.Add(L);
+            spec.SaveImage("qrss-AGCoff.png", intensity: 3);
+
+            var ffts = spec.GetFFTs();
+            double normalIntensity = 2;
+            for (int i = 0; i < ffts.Count; i++)
+            {
+                double[] sorted = new double[ffts[i].Length];
+                ffts[i].CopyTo(sorted, 0);
+                Array.Sort(sorted);
+
+                double percentile = 0.25;
+                int percentileIndex = (int)(percentile * ffts[0].Length);
+                double floorValue = sorted[percentileIndex];
+
+                for (int y = 0; y < ffts[i].Length; y++)
+                {
+                    ffts[i][y] = ffts[i][y] / floorValue * normalIntensity;
+                }
+
+                Console.WriteLine(floorValue);
+            }
+
+            spec.SaveImage("qrss-AGCon.png", intensity: 3);
+        }
+    }
+}
