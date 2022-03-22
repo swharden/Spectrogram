6ed42b4e706bd68a87aabeb0cbe2ff5076b94a75
Scott W Harden <swharden@gmail.com>
2022-03-22 08:11:42 -0400

    tests: demonstrate divide-by-zero error #42

---
+++ src/Spectrogram.Tests/AddTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectrogram.Tests
+{
+    internal class AddTests
+    {
+        [Test]
+        public void Test_No_Data()
+        {
+            SpectrogramGenerator sg = new(44100, 2048, 1000);
+            var bmp = sg.GetBitmap();
+        }
+    }
+}
